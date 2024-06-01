8196a0b02210c51c292b216546b91ea6c68a9fba
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-06-01 21:40:16 +0900

    setting

--- lua/themes/everforest-setting.lua
+++ lua/themes/everforest-setting.lua
@@ -1,6 +1,6 @@
 return {
   "sainnhe/everforest",
-  enabled = false,
+  enabled = true,
   lazy = false,
   priority = 1000,
   opts = {},
@@ -9,7 +9,7 @@ return {
 
     vim.g.everforest_background = "hard" -- hard, medium, soft
     vim.g.everforest_better_performance = 1
-    vim.g.everforest_transparent_background = 2 -- 1 background만 2 status line 같은 ui
+    vim.g.everforest_transparent_background = 0 -- 1 background만 2 status line 같은 ui
     vim.g.everforest_dim_inactive_windows = 1 -- neovim option, 비활성화 흐리게
     vim.g.everforest_ui_contrast = "high" -- 
     vim.g.everforest_diagnostic_text_highlight = 1 -- lsp 강조
