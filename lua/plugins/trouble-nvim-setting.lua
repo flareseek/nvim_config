d79f3e0df0a5a7479f4c51dee91481c9d2f03ca6
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-06-16 21:14:43 +0900

    setting

--- lua/plugins/trouble-nvim-setting.lua
+++ lua/plugins/trouble-nvim-setting.lua
@@ -1,6 +1,7 @@
 return {
   "folke/trouble.nvim",
-  branch = "dev", -- IMPORTANT!
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
     {
       "<leader>xx",
@@ -33,5 +34,5 @@ return {
       desc = "Quickfix List (Trouble)",
     },
   },
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
+
